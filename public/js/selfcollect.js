3e5bba8b4134e3d5a2463c30573e0b39fab45aa5
Syahmi Ghafar <92902180+FaridPuasa@users.noreply.github.com>
2022-05-07 14:22:38 +0800

    Update selfcollect.js

--- public/js/selfcollect.js
+++ public/js/selfcollect.js
@@ -26,29 +26,27 @@ function checkTrackingNum(field, autoMove) {
                 json_responsejd = JSON.parse(responsejd);
 
                 if (json_responsejd.status != 404) {
-                    if (assignTaskToAgent == 0) {
-                        request.open('POST', 'https://api.tookanapp.com/v2/assign_task');
-                        request.setRequestHeader('Content-Type', 'application/json');
+                    request.open('POST', 'https://api.tookanapp.com/v2/assign_task');
+                    request.setRequestHeader('Content-Type', 'application/json');
 
-                        request.onreadystatechange = function () {
-                            if (this.readyState === 4) {
-                                console.log('Status:', this.status);
-                                console.log('Headers:', this.getAllResponseHeaders());
-                                console.log('Body:', this.responseText);
+                    request.onreadystatechange = function () {
+                        if (this.readyState === 4) {
+                            console.log('Status:', this.status);
+                            console.log('Headers:', this.getAllResponseHeaders());
+                            console.log('Body:', this.responseText);
 
-                                localStorage.setItem("lastCS", document.getElementById("csName").value);
-                                document.getElementById("selfCollect").submit();
-                            }
-                        };
+                            localStorage.setItem("lastCS", document.getElementById("csName").value);
+                            document.getElementById("selfCollect").submit();
+                        }
+                    };
 
-                        var body = {
-                            'api_key': '51676580f24b091114132d38111925401ee4c2f328d978375e1f03',
-                            'job_id': document.getElementById("trackingNum").value,
-                            'fleet_id': document.getElementById("csName").value,
-                            'job_status': '2'
-                        };
-                        request.send(JSON.stringify(body));
-                    }
+                    var body = {
+                        'api_key': '51676580f24b091114132d38111925401ee4c2f328d978375e1f03',
+                        'job_id': document.getElementById("trackingNum").value,
+                        'fleet_id': document.getElementById("csName").value,
+                        'job_status': '2'
+                    };
+                    request.send(JSON.stringify(body));
                 }
 
                 if (json_responsejd.status == 404) {
