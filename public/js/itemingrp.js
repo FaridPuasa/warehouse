8fe0953cb835834780198baa47fd2c5a822fed2c
Syahmi Ghafar <92902180+FaridPuasa@users.noreply.github.com>
2022-05-16 15:25:00 +0800

    Update itemingrp.js

--- public/js/itemingrp.js
+++ public/js/itemingrp.js
@@ -1,6 +1,6 @@
 function checkTrackingNum(field, autoMove) {
     console.log(document.getElementById("trackingNum").value);
-    if (field.value.length >= field.value.length) {
+    if (field.value.length >= field.minLength) {
 
         document.getElementById("itemIn").style.display = 'none';
         document.getElementById("trackingnumberarea").style.display = 'none';
